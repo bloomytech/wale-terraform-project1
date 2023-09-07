3bd8d74507279995b814bdccae54b1965c98e160
elaw2k4 <elaw2k2@gmail.com>
2023-09-07 13:42:33 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-      region = "us-east-1"
-    }
-  }
-
+provider "aws" {
+region = "us-east-1"
+}
 
 resource "aws_vpc" "demotf_vpc" {
 cidr_block = "10.0.0.0/16"
